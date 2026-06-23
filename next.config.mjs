dbbcf18dd29d57846145a9794d34efb83516d407
Jahanzaib Shaikh <83217710+JahanzaibShaikh19@users.noreply.github.com>
2026-06-24 02:31:12 +0500

    chore: remove external screenshot host config

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "s.wordpress.com",
-        pathname: "/mshots/v1/**",
-      },
-    ],
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
