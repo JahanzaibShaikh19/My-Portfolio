8bec19901e1b4400d763085fc11e7b12475d0ccb
Jahanzaib Shaikh <83217710+JahanzaibShaikh19@users.noreply.github.com>
2026-06-24 02:34:29 +0500

    style: load final smoothness fixes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 import "./globals.css";
+import "./smooth-fixes.css";
 import Preloader from "@/components/Preloader";
 import AmbientBackground from "@/components/AmbientBackground";
 
